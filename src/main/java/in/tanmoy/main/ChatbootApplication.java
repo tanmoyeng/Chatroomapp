be98236febfad266cdaa794c62408103bc6a7d09
Tanmoy Jana <janatanmoy729@gmail.com>
2024-03-12 00:10:16 +0530

    1.0

---
+++ src/main/java/in/tanmoy/main/ChatbootApplication.java
@@ -0,0 +1,13 @@
+package in.tanmoy.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/tanmoy/main/ChatbootApplicationTests.java
@@ -0,0 +1,13 @@
+package in.tanmoy.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
